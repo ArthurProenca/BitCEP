26d7682c6d46d6e0e9349aa4163a5d0efd1b8c45
Guilherme Dias Cardoso <guilherme.dis@ufu.br>
2022-04-26 16:25:11 -0300

    Refactor, excludes importations not used

--- src/main/java/dev/bitway/bitcep/entity/dto/SearchProtocolResponse.java
+++ src/main/java/dev/bitway/bitcep/entity/dto/SearchProtocolResponse.java
@@ -4,12 +4,8 @@ import lombok.Data;
 
 import java.io.Serializable;
 import java.util.List;
+
 @Data
-//public class SearchProtocolResponse implements Serializable {
-//    private static final long serialVersionUID = 1L;
-//    private List<ProtocolResponse> protocolResponseList;
-//    private Long total;
-//}
 public class SearchProtocolResponse implements Serializable {
     public String sdvr01;
     public String sdnxtr;
